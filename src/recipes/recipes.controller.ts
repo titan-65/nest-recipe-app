fa0c5390f36fe519a7b001668382279949da7000
Vantol Bennett <vvantol2000@icloud.com>
2022-03-02 23:32:03 -0500

    updated Recipe Controller file with getAllRecipe and createRecipe

--- src/recipes/recipes.controller.ts
+++ src/recipes/recipes.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { RecipesService } from './recipes.service';
+import { RecipeCreateDto, RecipeDto } from './dto/recipe-create.dto';
 
 @Controller('recipes')
-export class RecipesController {}
+export class RecipesController {
+  constructor(private readonly recipesService: RecipesService) {}
+
+  @Get()
+  async getAllRecipes(): Promise<RecipeDto[]> {
+    return await this.recipesService.getAllRecipes();
+  }
+
+  @Post()
+  async createRecipe(@Body() recipeDto: RecipeCreateDto): Promise<RecipeDto> {
+    return await this.recipesService.createRecipe(recipeDto);
+  }
+}
